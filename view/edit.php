21b961284e2beaa27ddf7a1681e0ea672b317e53
jcaceres <jvcaceres17@gmail.com>
2025-04-03 16:20:27 -0500

    Fix: change lenguaje

--- view/edit.php
+++ view/edit.php
@@ -1,6 +1,6 @@
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Editar Suscripción</title>
--- view/index.php
+++ view/index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
