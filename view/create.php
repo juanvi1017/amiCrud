03b71239aeb02d0285e1e778023c7c998921680f
jcaceres <jvcaceres17@gmail.com>
2025-04-03 16:20:07 -0500

    Fix: Change lenguaje

--- view/create.php
+++ view/create.php
@@ -15,7 +15,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
